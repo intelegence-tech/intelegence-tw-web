72602db6bc53bd0644f9ff60528d62f17c2d7f75
yinghsun kao <yinghsun.kao@gmail.com>
2023-03-19 09:01:01 +0800

    add news api

--- python/web.py
+++ python/web.py
@@ -1,26 +1,67 @@
 import os
+import time
+import datetime
+import json
 import gspread
+import threading
 from oauth2client.service_account import ServiceAccountCredentials
 from bottle import route, run, request, static_file
 
 fileroot = os.environ['WEB_ROOT']
 sheet_id = os.environ['GS_SHEET_ID']
 
-print("WEB_ROOT: ", fileroot)
-print("GS_SHEET_ID: ", sheet_id)
+def log(*args):
+   print('[' + datetime.datetime.utcnow().replace(microsecond=0).isoformat() + ']', *args)
+
+log("WEB_ROOT: ", fileroot)
+log("GS_SHEET_ID: ", sheet_id)
 
 
-@route('/save', method='POST')
-def save():
-    data = request.forms
-    row = list(data.values())
+def gsheet(name):
     scopes = ["https://spreadsheets.google.com/feeds"]
     credentials = ServiceAccountCredentials.from_json_keyfile_name(
         ".google-api-key.json", scopes)
 
     client = gspread.authorize(credentials)
     spreadsheet = client.open_by_key(sheet_id)
-    sheet = spreadsheet.worksheet('data')
+    sheet = spreadsheet.worksheet(name)
+    return sheet
+
+
+cache_news = []
+
+def fetch_news():
+    log('Fetching news')
+    sheet = gsheet('news')
+    rows = sheet.get_all_records()
+    cache_news.insert(0,{ 'time': time.time(), 'news': rows})
+    del cache_news[1:]
+    log('Fetching done,', len(rows), 'rows')
+   
+
+@route('/api/news', method='GET')
+def news():
+  data = None
+  now = time.time()
+  fetch = True
+  if (len(cache_news) > 0):
+    data = cache_news[0]    
+    if (data['time'] > now - 86400):
+       fetch = False
+
+  if fetch:
+    t = threading.Thread(target=fetch_news)
+    t.start()
+    if (data is None):
+       t.join()
+       return json.dumps(cache_news[0])
+  return json.dumps(data)
+
+@route('/api/save', method='POST')
+def save():
+    data = request.forms
+    row = list(data.values())
+    sheet = gsheet('contact_us_data')
     sheet.append_row(row)
 
     return {'status': 'ok'}
